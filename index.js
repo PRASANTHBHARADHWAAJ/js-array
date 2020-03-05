8a37e1ce0059ca5ccfe2005e4d092fbbc72df428
PRASANTHBHARADHWAAJ <prasanth99waaj@gmail.com>
2020-03-05 16:07:43 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
-const arr1=["sdxs",12,56,"t"];
+const arr1=[{name:"sdxs",age:12},{name1:"sds",age1:56}];
+const arr=[1,2,3,4,5]
+let fe= arr1.forEach(function (val1) {
+ console.log(val1.name);
+})
+//returns sdxs
 
-const fe= arr1.forEach()
+//map
+const mapArray= arr.map(item =>
+{
+	return item=item * item;
+})
+console.log(mapArray)
+//returns [1, 4, 9, 16, 25]
+
+//filter
+const found=arr1.filter(item =>
+{
+return item.name == "sdxs";
+})
+console.log(JSON.stringify(found))
+//returns [{"name":"sdxs","age":12}]
+
+//includes returns true or false
+const found= arr.includes(5)
+console.log(found)
+
+const i = arr.indexOf(1)
+console.log(i)
+// 0
+
+const findind = arr1.findIndex(function (val) 
+{
+  return val.name == 'sdxs';
+})
+console.log(findind);
+//0
+
+//find
+const found=arr.find(item =>
+{
+return item == 2;
+})
+console.log(found);
+//2
+
+
+//slice
+const slice1 = arr.slice(1,4)
+console.log(slice1)
+//[2,3,4]
+
+//splice
+const splice1 = arr.splice()
+
+
+
+
+
+
+
+
+ 
+
+
